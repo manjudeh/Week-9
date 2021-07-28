269d3962122465ced487c088546ccc4ab42c229d
Back-end Class A <back-endclassa@DSIs-iMac.lan>
2021-07-28 11:51:44 +0100

    database

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config();
+
+
 port = process.env.PORT || 4500;
-const members = require('./Members/members');
+// const db = require('./config/db')
+
+
+
 
 app.use(express.json());
+
+
+
+ mongoose.connect('process.env.DB_CONNECT',
+    { useNewUrlParser: true },
+
+
+    () =>  console.log('connected to database')
+
+   
+
+);
+
+//import users
+const members = require('./Members/members');
 
 // app.get( '/api/users', (req,res) => {
 
@@ -13,6 +36,6 @@ app.use(express.json());
 
 const userroutes = require('./routes/userroutes');
 
-app.use('./api/routes', userroutes);
+app.use('/api/routes', userroutes);
 
 app.listen(port, () => console.log( `server running on ${port}`));
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const User = require('../models/usermodel');
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-    console.log(req.body);
+router.post ('/signup', async (req, res) => {
+     
+        let { firstName, lastName, email, password} = req.body
 
+        const checkEmail = await User.findOne({email});
+
+        if(checkEmail) {
+            return res.json({
+                status: "failed",
+                message: 'Email already exist',
+            });
+        }
+
+
+
+        const newUser = {firstName, lastName, email, password};
+
+        const createUser = await User.create(newUser);
+
+res.status(201).json({
+    status: 'success',
+    data: {
+        id: createUser._id,
+        firstName: createUser.fisrtName,
+        lastName: createUser.lastName,
+        email: createUser.email,
+    },
+})
+
+    
 });
 
 
@@ -11,4 +39,4 @@ router.post('/signup', (req, res) => {
 
 
 
-module.exports = router;
+module.exports = router;        
